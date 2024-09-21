636aba3d29dfb52d5e3283d6739a686b564a6cf1
ViniciusMendesp <vmp_tech@outlook.com>
2024-09-21 11:35:00 -0300

    feat: :sparkles: adding header component and lucide-react

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,8 +14,8 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
-      <body className={`${inter.className} antialiased`}>{children}</body>
+    <html className={`${inter.className} `} lang="pt">
+      <body className="bg-zinc-950 text-zinc-50 antialiased">{children}</body>
     </html>
   )
 }
