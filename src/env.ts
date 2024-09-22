f6ef233b5fbcae7840499fb3ce4baf439a102117
ViniciusMendesp <vmp_tech@outlook.com>
2024-09-21 21:21:34 -0300

    refactor: :recycle: env

--- src/env.ts
+++ src/env.ts
@@ -1,22 +1,14 @@
 import { z } from 'zod'
 
 const envSchema = z.object({
-  NEXT_PUBLIC_API_URL: z.string().url(),
+  NEXT_PUBLIC_API_BASE_URL: z.string().url(),
 })
-
 const parsedEnv = envSchema.safeParse(process.env)
-
 if (!parsedEnv.success) {
   console.error(
-    'Invalid environment variables:',
+    'Invalid environment variables',
     parsedEnv.error.flatten().fieldErrors,
   )
-
-  throw new Error('Invalid environment variables')
+  throw new Error('Invalid environment variables.')
 }
-
 export const env = parsedEnv.data
-
-// .flatten é um método que retorna um objeto com todos os erros de validação de uma forma mais legivel
-// .fieldErrors é um objeto com todos os erros de validação
-// zod é uma biblioteca de validação de tipos em typescript que permite validar objetos, strings, números, etc
