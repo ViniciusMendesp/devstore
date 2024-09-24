11fec1415ec1606a73a173ebb874023204fc0311
ViniciusMendesp <vmp_tech@outlook.com>
2024-09-24 10:33:18 -0300

    feat: :sparkles: finalizando o projeto

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreDuringBuilds: true,
+  },
   images: {
-    domains: ['github.com'],
+    domains: ['github.com', 'https://dev-store-api-ten.vercel.app/'],
   },
 }
 
